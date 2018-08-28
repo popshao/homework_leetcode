36e12a0c9fa65d2de1b42c9861302aa0cbf64d8d
lei.zhang <lei.zhang@rograndec.com>
2018-08-28 15:30:05 +0800

    代码测试通过

--- src/com/homework/Test20.java
+++ src/com/homework/Test20.java
@@ -1,47 +1,25 @@
 package com.homework;
 
-import java.util.Stack;
 
 public class Test20 {
 
     
     public static void main(String[] args) {
         Test20 test = new Test20();
-        System.out.println(test.isValid("(([]){})"));
     }
     
-    /**
-     * 只要遇到括号匹配的问题，我们就选择用栈，遇到左括号就进栈，遇到右括号，就判断栈顶元素是否与之匹配，匹配的话就pop出栈，不匹配的话就返回false
-     * 
-     * @param s
-     * @return
-     * @author lei.zhang
-     * @date 2018年8月27日
-     */
-    public boolean isValid(String s) {
-        if (s.length() % 2 != 0) {
-            return false;
-        }
-        Stack<Character> st = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (ch=='{' || ch =='[' || ch=='(' ) {
-                st.push(ch);
-            }else if (ch=='}' || ch ==']' || ch == ')' ){
-                if (st.empty()) return false;
-                char sch = st.lastElement();
-                if ( (sch=='{' && ch =='}') || (sch=='[' && ch==']') || (sch=='(' && ch==')' ) ){
-                    st.pop();
-                }else {
-                    return false;
-                }
-            }else{
-                return false;
-            }
-        }
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
         
-        return st.empty();
+        return l2;
     }
     
+    public class ListNode {
+        int      val;
+        ListNode next;
+        ListNode(int x) {
+            this.val = x;
+        }
+    }
 }
 
